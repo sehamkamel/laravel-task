8041f3a6ad6e5aff04ab24cb6327573a699e173c
Seham Kamel <sehamkamel2004@gmail.com>
2025-08-03 11:25:08 +0300

    initial commit for laravel task

---
+++ resources/views/notes/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('notes.layout')
+
+@section('content')
+
+<div class="card mt-5">
+    <h2 class="card-header">Edit Note</h2>
+    <div class="card-body">
+
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a class="btn btn-primary btn-sm" href="{{ route('notes.index') }}"><i class="fa fa-arrow-left"></i> Back</a>
+        </div>
+
+        <form action="{{ route('notes.update',$note->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="mb-3">
+                <label for="inputTitle" class="form-label"><strong>Title:</strong></label>
+            <input 
+    type="text" 
+    name="title" 
+    value="{{ old('title', $note->title) }}"
+    class="form-control @error('title') is-invalid @enderror" 
+    id="inputTitle" 
+    placeholder="Title">
+@error('title')
+    <div class="form-text text-danger">{{ $message }}</div>
+@enderror
+
+            </div>
+
+            <div class="mb-3">
+                <label for="inputcontent" class="form-label"><strong>content:</strong></label>
+             <textarea 
+    name="content"
+    class="form-control @error('content') is-invalid @enderror" 
+    style="height:150px"  
+    id="inputcontent" 
+    placeholder="content">{{ old('content', $note->content) }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Update</button>
+        </form>
+
+    </div>
+</div>
+@endsection
